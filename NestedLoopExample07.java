d358a9b2b6c095d78fee377a1120b1072c7029cd
swatikimari03 <125912349+swatikimari03@users.noreply.github.com>
2026-01-21 06:21:08 -0800

    Add files via upload

---
+++ NestedLoopExample07.java
@@ -0,0 +1,19 @@
+public class NestedLoopExample07 {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 9, 9}
+        } ;
+
+        for( int i = 0; i < arr.length; i++) {
+            for ( int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+    
+}
